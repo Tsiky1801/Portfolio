3d49b678f09d28a78898ad25c5b657e2efeb82ad
Tsiky1801 <tsikynambs@gmail.com>
2026-02-20 07:31:57 +0300

    Implement mobile menu and scroll animation

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-// Animation simple au scroll
+// MENU MOBILE
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+// ACTIVE LINK
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", function () {
+        links.forEach(l => l.classList.remove("active"));
+        this.classList.add("active");
+        navLinks.classList.remove("show");
+    });
+});
+
+// ANIMATION AU SCROLL
 const sections = document.querySelectorAll(".section");
 
 window.addEventListener("scroll", () => {
     sections.forEach(section => {
-        const position = section.getBoundingClientRect().top;
-        const screenHeight = window.innerHeight;
-
-        if (position < screenHeight - 100) {
-            section.style.opacity = "1";
-            section.style.transform = "translateY(0)";
+        const sectionTop = section.getBoundingClientRect().top;
+        if(sectionTop < window.innerHeight - 100){
+            section.classList.add("show");
         }
     });
 });
